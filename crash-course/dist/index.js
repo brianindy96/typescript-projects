8c122ea8700967bb0511881785a18dcdcf73abd8
Brian Garton <gartonb@gmail.com>
2023-03-09 16:28:35 +0100

    Arrays and Tuples

--- crash-course/dist/index.js
+++ crash-course/dist/index.js
@@ -1,3 +1,26 @@
 "use strict";
-let id = 5;
-console.log('ID:', id);
+// Basic Types
+let id = 6;
+let company = 'Brian Garton';
+let isPublished = true;
+let x = 'Hello';
+let age = 30;
+let arrays = ['one', 'two', 'three'];
+let arr = [1, true, 'hello'];
+arrays.push('four');
+// ['one', 'two', 'three', 'four']
+// arrays.push(2); // Error
+console.log(arrays);
+//Tuple
+let person = [1, 'Brian', true];
+// If it comes out of order, it will throw an error
+// let wrongOrder: [number, string, boolean] = ['Brian', 1 true];
+// Error
+// Tuple Array
+let employee; // arrays of tuples
+employee = [
+    [1, 'Brian'],
+    [2, 'John'],
+    [3, 'Sara'],
+];
+// Union
--- crash-course/src/index.ts
+++ crash-course/src/index.ts
@@ -1,3 +1,31 @@
-let id: number = 5
+// Basic Types
+let id: number = 6;
+let company: string = 'Brian Garton';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+let age: number = 30;
+let arrays: string[] = ['one', 'two', 'three'];
+let arr : any[] = [1, true, 'hello'];
 
-console.log('ID:', id)
+
+arrays.push('four');
+// ['one', 'two', 'three', 'four']
+// arrays.push(2); // Error
+console.log(arrays);
+
+//Tuple
+let person: [number, string, boolean] = [1, 'Brian', true];
+// If it comes out of order, it will throw an error
+// let wrongOrder: [number, string, boolean] = ['Brian', 1 true];
+// Error
+
+// Tuple Array
+let employee: [number, string][]; // arrays of tuples
+
+employee = [
+    [1, 'Brian'],
+    [2, 'John'],
+    [3, 'Sara'],
+]
+
+// Union
