26c144b6efd85fee2722f97bd04e096708812250
Brian Garton <gartonb@gmail.com>
2023-03-11 10:28:29 +0100

    SingleSelectLogic done

--- select-with-typescript/src/components/Select.tsx
+++ select-with-typescript/src/components/Select.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import styles from "./select.module.css"
 
 type SelectOption = {
   label: string
-  value: any
+  value: string | number
 }
 
 type SelectProps = {
@@ -31,13 +31,18 @@ const Select = ({ options, value, onChange } : SelectProps) => {
 
   // select option choice as value
   const selectOption = (option: SelectOption) => {
-    onChange(option);
+    if (option !== value) onChange(option);
   }
 
   // isOptionSelected
   const isOptionSelected = (option: SelectOption) => {
     return option === value;
   }
+
+  // setsHighlightedIndex to default 0 everytime div is left and entered again
+  useEffect(() => {
+    if(showOptions) setHighlightedIndex(0);
+  }, [showOptions])
 
 
   return (
@@ -63,7 +68,7 @@ const Select = ({ options, value, onChange } : SelectProps) => {
         <ul className={`${styles.options} ${showOptions ? styles.show : ""}`}>
           {options.map((option, index) => (
             <li 
-            key={option.label} 
+            key={option.value} 
             onClick={e => {
               e.stopPropagation();
               selectOption(option);
@@ -82,6 +87,6 @@ const Select = ({ options, value, onChange } : SelectProps) => {
         </ul>
     </div>
   )
-}
+} 
 
 export default Select
