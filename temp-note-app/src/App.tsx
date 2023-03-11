b38124e6a1cfe6f62cb940cf74ca47247afc0185
Brian Garton <gartonb@gmail.com>
2023-03-11 17:11:26 +0100

    NoteForm.tsx added

--- temp-note-app/src/App.tsx
+++ temp-note-app/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom"
+import { Container } from 'react-bootstrap'
+import NewNote from './pages/NewNote'
 
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<div>Show</div>} />
+          <Route path="edit" element={<div>Edit</div>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   )
 }
 
