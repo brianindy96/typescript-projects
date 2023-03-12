c47cc73eec8ade1ed52e8c7eed36880e92c60a5c
Brian Garton <gartonb@gmail.com>
2023-03-12 07:13:17 +0100

    Delete functionality implemented

--- temp-note-app/src/App.tsx
+++ temp-note-app/src/App.tsx
@@ -68,6 +68,12 @@ function App() {
       })
     })
   }
+
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
   
   function addTag(tag: Tag){
     setTags(prev => [...prev, tag]);
@@ -87,7 +93,7 @@ function App() {
         path="/:id"
         element={<NoteLayout notes={notesWithTags} />}
         >
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route path="edit" element={<EditNote onSubmit={onUpdateNote}
         onAddTag={addTag}
         availableTags={tags} />} />
