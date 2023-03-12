228860081ca1afe3a1a37474430b45a578717938
Brian Garton <gartonb@gmail.com>
2023-03-12 06:08:56 +0100

    NoteCard styled

--- temp-note-app/src/components/Notecard.tsx
+++ temp-note-app/src/components/Notecard.tsx
@@ -1,4 +1,7 @@
+import { Badge, Card, Stack } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import { Tag } from "../App"
+import styles from "./NoteList.modules.css"
 
 export type SimplifiedNotes = {
     title: string
@@ -8,7 +11,25 @@ export type SimplifiedNotes = {
 
 const NoteCard = ({ title, id, tags }: SimplifiedNotes) => {
   return (
-    <div>Notecard</div>
+    <Card 
+    as={Link} to={`/${id}`}
+    className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+        <Card.Body>
+            <Stack gap={2} className="align-items-center justify-content-center h-100">
+                <Card.Title>{title}</Card.Title>
+                {tags.length > 0 && (
+                    <Stack className="justify-content-center flex-wrap" gap={1} direction="horizontal">
+                        {tags.map(tag => (
+                            <Badge className="text-truncate" key={tag.id}>{tag.label}</Badge>
+                        ))}  
+                    </Stack>
+                )}   
+            </Stack>
+        </Card.Body>
+        
+
+    </Card>
   )
 }
 
